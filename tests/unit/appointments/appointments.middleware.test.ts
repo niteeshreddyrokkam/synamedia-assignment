c4426ac8f876f1ddfefd38851fa30ff44a03cc61
Niteesh Rokkam <niteesh.rokkam@dazn.com>
2024-11-24 15:26:24 +0530

    Add remaining tests

--- tests/unit/appointments/appointments.middleware.test.ts
+++ tests/unit/appointments/appointments.middleware.test.ts
@@ -1,6 +1,17 @@
 import { Request, Response, NextFunction } from 'express';
-import { isTimeSlotValid, validateBody } from '../../../src/appointments/appointments.middleware';
+import { isTimeSlotValid, validateBody, validateBookAppointment, validateCancelAppointmentReq, validateUpdateAppointmentReq } from '../../../src/appointments/appointments.middleware';
 import { STATUS_CODES } from '../../../src/constants';
+import { appointments } from '../../../src/types';
+jest.mock('../../../src/types', () => ({
+    appointments: [
+      {
+        patient: { email: 'test@example.com' },
+        doctorName: 'Dr.ABC',
+        timeSlot: '10:00 AM - 11:00 AM',
+      },
+    ],
+    availableDoctors: ['Dr.ABC', 'Dr. Johnson'],
+  }));
 
 jest.mock('../../../src/validation-schemas/appointment', () => ({
   vadlidateBookAppointment: jest.fn(() => ({ valid: true, errors: [] })),
@@ -25,7 +36,7 @@ describe('validateBody function', () => {
       body: {
         firstName: "John",
         lastName: "Doe",
-        email: "john.doe@example.com1",
+        email: "test@example.com1",
         timeSlot: "12:00 PM - 1:00 PM",
         doctorName: "Dr.ABC"          
       },
@@ -61,7 +72,7 @@ describe('validateBody function', () => {
     req.method = 'PUT';
     req.url = '/updateAppointment';
     req.body = {
-        email: "john.doe@example.com1",
+        email: "test@example.com1",
         originalTimeSlot: "12:00 PM - 1:00 PM",
         newTimeSlot: "2:00 PM - 3:00 PM"
     };      
@@ -107,4 +118,162 @@ describe('validateBody function', () => {
     await validateBody(req, res, next);
     expect(next).toHaveBeenCalledTimes(1);
   });
+});
+
+describe('validateUpdateAppointmentReq', () => {
+let req: Request;
+let res: Response;
+let next: NextFunction;
+beforeEach(() => {
+    req = {
+    body: {},
+    } as Request;
+    res = {
+    status: jest.fn().mockReturnThis(),
+    json: jest.fn(),
+    } as unknown as Response;
+    next = jest.fn();
+});
+afterEach(() => {
+    jest.clearAllMocks();
+});
+it('should return 404 if appointment not found', async () => {
+    req.body = {
+    email: 'non-existent@example.com',
+    originalTimeSlot: '10:00 AM - 11:00 AM',
+    newTimeSlot: '11:00 AM - 12:00 PM',
+    };
+    await validateUpdateAppointmentReq(req, res, next);
+    expect(res.status).toHaveBeenCalledTimes(1);
+    expect(res.status).toHaveBeenCalledWith(STATUS_CODES.NOT_FOUND);
+    expect(res.json).toHaveBeenCalledTimes(1);
+    expect(res.json).toHaveBeenCalledWith({ message: 'Original appointment not found.' });
+});
+it('should return 409 if new time slot already booked', async () => {
+    req.body = {
+    email: 'test@example.com',
+    originalTimeSlot: '10:00 AM - 11:00 AM',
+    newTimeSlot: '10:00 AM - 11:00 AM',
+    };
+    await validateUpdateAppointmentReq(req, res, next);
+    expect(res.status).toHaveBeenCalledTimes(1);
+    expect(res.status).toHaveBeenCalledWith(STATUS_CODES.CONFLICT);
+    expect(res.json).toHaveBeenCalledTimes(1);
+    expect(res.json).toHaveBeenCalledWith({ message: 'New time slot already booked.' });
+});
+it('should call next if valid update appointment request', async () => {
+    req.body = {
+    email: 'test@example.com',
+    originalTimeSlot: '10:00 AM - 11:00 AM',
+    newTimeSlot: '11:00 AM - 12:00 PM',
+    };
+    await validateUpdateAppointmentReq(req, res, next);
+    expect(next).toHaveBeenCalledTimes(1);
+});
+});
+
+describe('validateCancelAppointmentReq', () => {
+let req: Request;
+let res: Response;
+let next: NextFunction;
+beforeEach(() => {
+    req = {
+    body: {},
+    } as Request;
+    res = {
+    status: jest.fn().mockReturnThis(),
+    json: jest.fn(),
+    } as unknown as Response;
+    next = jest.fn();
+});
+afterEach(() => {
+    jest.clearAllMocks();
+});
+it('should return bad request if email or timeSlot is missing', async () => {
+    req.body = { email: 'test@example.com' };
+    await validateCancelAppointmentReq(req, res, next);
+    expect(res.status).toHaveBeenCalledTimes(1);
+    expect(res.status).toHaveBeenCalledWith(STATUS_CODES.BAD_REQUEST);
+    expect(res.json).toHaveBeenCalledTimes(1);
+    expect(res.json).toHaveBeenCalledWith({ message: 'All fields are required.' });
+});
+it('should return not found if appointment is not found', async () => {
+    req.body = { email: 'test@example.com', timeSlot: '10:00 AM - 11:00 AM' };
+    appointments.findIndex = jest.fn().mockReturnValue(-1);
+    await validateCancelAppointmentReq(req, res, next);
+    expect(res.status).toHaveBeenCalledTimes(1);
+    expect(res.status).toHaveBeenCalledWith(STATUS_CODES.NOT_FOUND);
+    expect(res.json).toHaveBeenCalledTimes(1);
+    expect(res.json).toHaveBeenCalledWith({ message: 'Appointment not found.' });
+});
+it('should call next if appointment is found', async () => {
+    req.body = { email: 'test@example.com', timeSlot: '10:00 AM - 11:00 AM' };
+    appointments.findIndex = jest.fn().mockReturnValue(0);
+    await validateCancelAppointmentReq(req, res, next);
+    expect(next).toHaveBeenCalledTimes(1);
+});
+});
+
+describe('validateBookAppointment', () => {
+let req: Request;
+let res: Response;
+let next: NextFunction;
+beforeEach(() => {
+    req = {
+    body: {},
+    } as Request;
+    res = {
+    status: jest.fn().mockReturnThis(),
+    json: jest.fn(),
+    } as unknown as Response;
+    next = jest.fn();
+});
+afterEach(() => {
+    jest.clearAllMocks();
+});
+it('should return 400 for invalid doctor name', async () => {
+    req.body = {
+        email: 'test@example.com',
+        timeSlot: '10:00 AM - 11:00 AM',
+        doctorName: 'Invalid Doctor',
+    };
+    await validateBookAppointment(req, res, next);
+    expect(res.status).toHaveBeenCalledTimes(1);
+    expect(res.status).toHaveBeenCalledWith(STATUS_CODES.BAD_REQUEST);
+    expect(res.json).toHaveBeenCalledTimes(1);
+    expect(res.json).toHaveBeenCalledWith({ message: 'Invalid doctor name.' });
+});
+it('should return 409 for time slot already booked', async () => {
+    req.body = {
+        email: 'test@example.com',
+        timeSlot: '10:00 AM - 11:00 AM',
+        doctorName: 'Dr.ABC',
+    };
+    await validateBookAppointment(req, res, next);
+    expect(res.status).toHaveBeenCalledTimes(1);
+    expect(res.status).toHaveBeenCalledWith(STATUS_CODES.CONFLICT);
+    expect(res.json).toHaveBeenCalledTimes(1);
+    expect(res.json).toHaveBeenCalledWith({ message: 'Time slot already booked.' });
+});
+it('should return 409 for appointment already exists at the same time slot', async () => {
+    req.body = {
+        email: 'test@example.com',
+        timeSlot: '10:00 AM - 11:00 AM',
+        doctorName: 'Dr.XYZ',
+    };
+    await validateBookAppointment(req, res, next);
+    expect(res.status).toHaveBeenCalledTimes(1);
+    expect(res.status).toHaveBeenCalledWith(STATUS_CODES.CONFLICT);
+    expect(res.json).toHaveBeenCalledTimes(1);
+    expect(res.json).toHaveBeenCalledWith({ message: 'An Appointment already exists at this time slot' });
+});
+it('should call next() for valid booking request', async () => {
+    req.body = {
+        email: 'test1@example.com',
+        timeSlot: '10:00 AM - 11:00 AM',
+        doctorName: 'Dr.XYZ',
+    };
+    await validateBookAppointment(req, res, next);
+    expect(next).toHaveBeenCalledTimes(1);
+});
 });
